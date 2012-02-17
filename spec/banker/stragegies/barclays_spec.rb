252ba5dee18d36e6613042c6a10891de782086ea
Kyle Welsby <kyle@mekyle.com>
2012-02-17 22:58:07 +0000

    fix file paths

--- spec/banker/stragegies/barclays_spec.rb
+++ spec/banker/stragegies/barclays_spec.rb
@@ -1,21 +1,28 @@
 require 'spec_helper'
 
 describe Banker::Stratagies::Barclays do
+  let(:basic_access_start) { File.read(File.expand_path('../../../support/barclays/BasicAccessStart.do.html',__FILE__)) }
+  let(:basic_access_step1) { File.read(File.expand_path('../../../support/barclays/BasicAccessStep1.do.html',__FILE__)) }
+  let(:basic_access_step2) { File.read(File.expand_path('../../../support/barclays/BasicAccessStep2.do.html',__FILE__)) }
+  let(:redirect) { File.read(File.expand_path('../../../support/barclays/Redirect.do.html',__FILE__)) }
+  let(:export_data1) { File.read(File.expand_path('../../../support/barclays/ExportData1.do.html',__FILE__)) }
+  let(:data) { File.read(File.expand_path('../../../support/barclays/data.ofx',__FILE__)) }
+
   before do
-    stub_http_request(:get, "https://ibank.barclays.co.uk/olb/y/BasicAccessStart.do").to_return(:status => 200, :body => File.read('spec/support/barclays/BasicAccessStart.do.html'), :headers => {'Content-Type' => 'text/html'})
+    stub_http_request(:get, "https://ibank.barclays.co.uk/olb/y/BasicAccessStart.do").to_return(:status => 200, :body => basic_access_start, :headers => {'Content-Type' => 'text/html'})
 
-    stub_http_request(:post, "https://ibank.barclays.co.uk/olb/y/BasicAccessStep1.do").to_return(:status => 200, :body => File.read('spec/support/barclays/BasicAccessStep1.do.html'), :headers => {'Content-Type' => 'text/html'})
+    stub_http_request(:post, "https://ibank.barclays.co.uk/olb/y/BasicAccessStep1.do").to_return(:status => 200, :body => basic_access_step1, :headers => {'Content-Type' => 'text/html'})
 
-    stub_http_request(:post, "https://ibank.barclays.co.uk/olb/y/BasicAccessStep2.do").to_return(:status => 200, :body => File.read('spec/support/Barclays/BasicAccessStep2.do.html'), :headers => {'Content-Type' => 'text/html'})
+    stub_http_request(:post, "https://ibank.barclays.co.uk/olb/y/BasicAccessStep2.do").to_return(:status => 200, :body => basic_access_step2, :headers => {'Content-Type' => 'text/html'})
   end
 
   subject {Banker::Stratagies::Barclays.new(:surname => 'Bloggs',
-                                             :date_of_birth => Date.parse('2012-01-01'),
-                                             :memorable_word => "superduper",
-                                             :card_number => 4111111111111111)}
+                                            :date_of_birth => Date.parse('2012-01-01'),
+                                            :memorable_word => "superduper",
+                                            :card_number => 4111111111111111)}
 
   describe '.new' do
-    
+
     it{ subject.surname.should eql 'Bloggs' }
 
     it{ subject.date_of_birth.should eql Date.parse('2012-01-01') }
@@ -37,14 +44,14 @@ describe Banker::Stratagies::Barclays do
 
   describe '#get_data' do
     before do
-      stub_request(:get, "https://ibank.barclays.co.uk/olb/y/Redirect.do?go=ExportData1.do%3Faction%3DExport%2BBank%2BStatement%7C%7CExport%2BData&Go=Go").to_return(:status => 200, :body => File.read('spec/support/Barclays/Redirect.do.html'), :headers => {'Content-Type' => 'text/html'})
+      stub_request(:get, "https://ibank.barclays.co.uk/olb/y/Redirect.do?go=ExportData1.do%3Faction%3DExport%2BBank%2BStatement%7C%7CExport%2BData&Go=Go").to_return(:status => 200, :body => redirect, :headers => {'Content-Type' => 'text/html'})
 
-      stub_request(:post, 'https://ibank.barclays.co.uk/olb/y/ExportData1.do').to_return(:status => 200, :body => File.read('spec/support/Barclays/ExportData1.do.html'), :headers => {'Content-Type' => 'text/html'})
+      stub_request(:post, 'https://ibank.barclays.co.uk/olb/y/ExportData1.do').to_return(:status => 200, :body => export_data1, :headers => {'Content-Type' => 'text/html'})
 
-      stub_request(:post, 'https://ibank.barclays.co.uk/olb/y/ExportData5.do').to_return(:status => 200, :body => File.read('spec/support/Barclays/data.ofx'), :headers => {'Content-Type' => 'text/html'})
+      stub_request(:post, 'https://ibank.barclays.co.uk/olb/y/ExportData5.do').to_return(:status => 200, :body => data, :headers => {'Content-Type' => 'text/html'})
     end
     it 'should receive account balance' do
-      
+
       subject.get_data.account.balance.amount.should eql 4100.1
     end
   end
