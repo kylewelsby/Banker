3393e0a0e9a535772d02b688b3f838a1fd4fe786
Kyle Welsby <kyle@mekyle.com>
2012-12-11 12:07:47 +0000

    more techniques to tie or win

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,20 @@ end
 
 require 'rspec'
 require 'webmock/rspec'
+require 'vcr'
 require 'banker'
 
 RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
   config.order = :rand
   config.color_enabled = true
 
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = { :record => :none }
 end
